65219fa305eae9d19e41eebb0aef1901168fb98f
[JAM] Andreas <andreas.edmeier@web.de>
2020-10-03 13:24:15 +0200

    created barebone BugDirector

---
+++ BugInALoop/Assets/Code/Runtime/Behaviours/BugDirector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BIAL {
+	public class BugDirector : MonoBehaviour {
+
+		[SerializeField] SpawnStrategy strategy = null;
+		[SerializeField] BugFactory factory = null;
+
+		float timeCollapsed = 0;
+
+		private void Start() {
+			timeCollapsed = 0;
+		}
+
+		private void FixedUpdate() {
+			timeCollapsed += Time.fixedDeltaTime;
+
+			while(strategy.ShouldSpawnNextBug(timeCollapsed)) {
+				strategy.SpawnNext(factory.GetNextBug());
+			}
+		}
+	}
+}
