2a4b226a109f526710e85f5758c4cb136382d84a
Lucas <saculucas98@aol.com>
2020-10-04 13:07:58 +0200

    Added Entity Spawn Position/Rotation mechanics

--- BugInALoop/Assets/Code/Runtime/Behaviours/SpawningDirectors/SpawningDirector.cs
+++ BugInALoop/Assets/Code/Runtime/Behaviours/SpawningDirectors/SpawningDirector.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using BIAL.Entities;
 using BIAL.Runtime.DataStorage;
 using BIAL.Singletons;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace BIAL.Runtime.Spawning
 {
@@ -44,9 +47,47 @@ namespace BIAL.Runtime.Spawning
 			{
 				foreach (EntityConfig config in GetEntitiesToSpawn())
 				{
-					EntityStateManager.AddActiveEntity(config.Factory.CreateNewEntity(config));
+					CreateNewEntity(config);
 				}
 			}
+		}
+
+		private static void CreateNewEntity(EntityConfig config)
+		{
+			Entity newEntity = config.Factory.CreateNewEntity(config);
+
+			Vector2 startPosition = GetRandomSpawnPosition();
+			Vector2 walkPosition = GetRandomWalkPosition();
+			Vector2 posDifference = walkPosition - startPosition;
+			
+			newEntity.transform.position = new Vector3(startPosition.x, LevelSettings.Current.EntitySpawnHeight, startPosition.y);
+			newEntity.transform.eulerAngles = new Vector3(0,0,Mathf.Atan2(posDifference.y, posDifference.x) * Mathf.Rad2Deg);
+
+			EntityStateManager.AddActiveEntity(newEntity);
+		}
+
+		private static Vector2 GetRandomSpawnPosition()
+		{
+			float xOff = Random.value * LevelSettings.Current.SpawnDistance;
+			float yOff = Random.value * LevelSettings.Current.SpawnDistance;
+
+			int xSide = Random.value > 0.5f ? 1 : -1;
+			int ySide = Random.value > 0.5f ? 1 : -1;
+			
+			Rect rect = LevelSettings.Current.NonSpawnableRect;
+			return new Vector2(rect.position.x + rect.width / 2 * xSide + xOff * xSide,
+								rect.position.y + rect.height / 2 * ySide + yOff * ySide);
+		}
+
+		private static Vector2 GetRandomWalkPosition()
+		{
+			Rect walkRect = LevelSettings.Current.EntityTargetWalkRect;
+			float lowerX = walkRect.position.x - walkRect.width / 2;
+			float upperX = walkRect.position.x + walkRect.width / 2;
+			float lowerY = walkRect.position.y - walkRect.height / 2;
+			float upperY = walkRect.position.y + walkRect.height / 2;
+			
+			return new Vector2(Random.Range(lowerX, upperX), Random.Range(lowerY, upperY));
 		}
 
 		protected virtual void PostDirectorAwake() { }
--- BugInALoop/Assets/Code/Runtime/DataStorage/ScriptableObjects/LevelSettings.cs
+++ BugInALoop/Assets/Code/Runtime/DataStorage/ScriptableObjects/LevelSettings.cs
@@ -5,6 +5,12 @@ namespace BIAL.Runtime.DataStorage
 {
 	public class LevelSettings : ScriptableObject
 	{
+		public static LevelSettings Current { get; set; }
+
+		public Rect EntityTargetWalkRect;
+		public Rect NonSpawnableRect;
+		public float SpawnDistance;
+		public float EntitySpawnHeight;
 		public SpawningDirector[] StartSpawningDirectors;
 	}
 }
