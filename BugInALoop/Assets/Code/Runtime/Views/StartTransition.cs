8e4820b7d1f0af666267374e3c2eecc2bcc90173
[JAM] Andreas <andreas.edmeier@web.de>
2020-10-04 13:35:14 +0200

    added Camera Transition

---
+++ BugInALoop/Assets/Code/Runtime/Views/StartTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BIAL.Runtime {
+	public class StartTransition : MonoBehaviour {
+		[SerializeField] Transform startPos = null;
+		[SerializeField] Transform endPos = null;
+		[SerializeField] float time = 0;
+
+		void Start() {
+			Camera.main.transform.position = startPos.position;
+			Camera.main.transform.rotation = startPos.rotation;
+			Singletons.Transition.s_instance.TriggerTransition(endPos, time);
+			StartCoroutine(IEChangeSzene(Scene.menu, time));
+		}
+
+		IEnumerator IEChangeSzene(Scene scene, float delay) {
+			yield return new WaitForSeconds(delay);
+			BehaviourFacade.s_instance.currentScene.value = scene;
+			Destroy(this);
+		}
+	}
+}
---
+++ BugInALoop/Assets/Code/Runtime/Views/Transition.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AsserTOOLres;
+
+namespace BIAL.Singletons {
+	public class Transition : Singleton<Transition> {
+		Vector3 startPos;
+		Quaternion startRot;
+		Vector3 endPos;
+		Quaternion endRot;
+		float endTime = 0;
+		float startTime = 0;
+		bool isInTransition = false;
+
+		public void TriggerTransition(Transform end, float time) {
+			startPos = Camera.main.transform.position;
+			startRot = Camera.main.transform.rotation;
+			endPos = end.position;
+			endRot = end.rotation;
+			startTime = Time.time;
+			endTime = startTime + time;
+			isInTransition = true;
+		}
+
+		void Update() {
+			if(!isInTransition) {
+				return;
+			}
+			if(Time.time > endTime) {
+				isInTransition = false;
+				Camera.main.transform.position = endPos;
+				Camera.main.transform.rotation = endRot;
+				return;
+			}
+
+			float delta = (Time.time - startTime) / (endTime - startTime);
+			delta = Mathf.Clamp01(delta);
+			Camera.main.transform.position = Vector3.Lerp(startPos, endPos, delta);
+			Camera.main.transform.rotation = Quaternion.Lerp(startRot, endRot, delta);
+		}
+	}
+}
