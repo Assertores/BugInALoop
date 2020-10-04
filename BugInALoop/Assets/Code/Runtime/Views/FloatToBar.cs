a74068d973d7de3b1506cbd525fb2b9a526e0f5b
[JAM] Andreas <andreas.edmeier@web.de>
2020-10-04 15:08:06 +0200

    updated logic prefab

--- BugInALoop/Assets/Code/Runtime/Views/FloatToBar.cs
+++ BugInALoop/Assets/Code/Runtime/Views/FloatToBar.cs
@@ -7,8 +7,7 @@ namespace BIAL.Runtime
 	{
 		[SerializeField] private Image bar = null;
 		[SerializeField] private OFloatIdentifier type = OFloatIdentifier.Ink;
-		[SerializeField] private float maxValue = 0;
-		[SerializeField] private bool shouldScale = true;
+		[SerializeField] private OFloatIdentifier maxValue = OFloatIdentifier.MaxInk;
 
 		private void Start()
 		{
@@ -24,14 +23,15 @@ namespace BIAL.Runtime
 			BehaviourFacade.s_instance.Floats[(int) type] += OnChange;
 		}
 
+		private void OnDestroy() {
+			if(BehaviourFacade.Exists()) {
+				BehaviourFacade.s_instance.Floats[(int)type] -= OnChange;
+			}
+		}
+
 		private void OnChange()
 		{
-			if (shouldScale && (BehaviourFacade.s_instance.Floats[(int) type].value > maxValue))
-			{
-				maxValue = BehaviourFacade.s_instance.Floats[(int) type].value;
-			}
-
-			bar.fillAmount = Mathf.Clamp(BehaviourFacade.s_instance.Floats[(int) type].value / maxValue, 0, 1);
+			bar.fillAmount = Mathf.Clamp(BehaviourFacade.s_instance.Floats[(int) type].value / BehaviourFacade.s_instance.Floats[(int)maxValue].value, 0, 1);
 		}
 	}
 }
