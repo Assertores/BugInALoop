bd900bb65b65843a0bd97fa9e7ddc4c0eef2d73e
[JAM] Andreas <andreas.edmeier@web.de>
2020-10-04 15:15:51 +0200

    added ActivateInScene script

---
+++ BugInALoop/Assets/Code/Runtime/Views/ActivateInScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AsserTOOLres;
+
+namespace BIAL.Runtime {
+	public class ActivateInScene : MonoBehaviour {
+		[SerializeField] Scene scene;
+		private void Awake() {
+			BehaviourFacade.s_instance.CurrentScene += OnChange;
+			OnChange(BehaviourFacade.s_instance.CurrentScene);
+		}
+
+		private void OnDestroy() {
+			if(BehaviourFacade.Exists()) {
+				BehaviourFacade.s_instance.CurrentScene -= OnChange;
+			}
+		}
+
+		void OnChange(Observable<Scene> element) {
+			gameObject.SetActive(element.value == scene);
+		}
+	}
+}
